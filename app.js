d3ec986758e94d3db2ff28a8f31246a6d27ddcee
xiaof <xiaof@wangsu.com>
2018-04-14 18:25:19 +0800

    update

--- app.js
+++ app.js
@@ -13,9 +13,33 @@ App({
     }else{
       //调用登录接口
       wx.login({
-        success: function () {
+        success: function (res) {
+          wx.request({
+            url: 'http://172.28.11.40:8000/users/getuserinfo/',
+            data: { "data": res.code },  // the code is a mock one
+            method: 'POST', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+            // header: {}, // 设置请求的 header
+            success: function (res) {
+              console.log('success')
+              that.globalData.openid = res.data.openid
+              console.log(res.data.openid)
+              // success
+            },
+            fail: function (res) {
+              console.log('fail')
+              console.log(res)
+              // fail
+            },
+            complete: function (res) {
+              console.log('complete')
+              console.log(res)
+              // complete
+            }
+          })
           wx.getUserInfo({
             success: function (res) {
+              console.log(res.userInfo)
+              console.log(res.encryptedData)
               that.globalData.userInfo = res.userInfo
               typeof cb == "function" && cb(that.globalData.userInfo)
             }
@@ -25,6 +49,26 @@ App({
     }
   },
   globalData:{
-    userInfo:null
+    userInfo:null,
+    openid: null
   }
 })
+// App({
+//   onLaunch: function () {
+//     wx.login({
+//       success: function (res) {
+//         if (res.code) {
+//           //发起网络请求
+//           wx.request({
+//             url: 'https://test.com/onLogin',
+//             data: {
+//               code: res.code
+//             }
+//           })
+//         } else {
+//           console.log('登录失败！' + res.errMsg)
+//         }
+//       }
+//     });
+//   }
+// })
